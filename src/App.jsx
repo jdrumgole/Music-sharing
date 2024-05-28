62ee924847ec3e5f079e92911ceff9277ef53648
jdrumgole <85814044+jdrumgole@users.noreply.github.com>
2024-05-28 08:04:29 -0400

    new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/Components/Background/Background.css 	new file:   src/Components/Background/Background.jsx 	new file:   src/Components/Hero/Hero.css 	new file:   src/Components/Hero/Hero.jsx 	new file:   src/Components/Navbar/Navbar.css 	new file:   src/Components/Navbar/Navbar.jsx 	new file:   src/assets/arrow_btn.png 	new file:   src/assets/image1.png 	new file:   src/assets/image2.png 	new file:   src/assets/image3.png 	new file:   src/assets/pause_icon.png 	new file:   src/assets/play_icon.png 	new file:   src/assets/video1.mp4 	new file:   src/index.css 	new file:   src/main.jsx

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import Background from "./Components/Background/Background.jsx";
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import Hero from "./Components/Hero/Hero.jsx";
+
+
+const App = () => {
+  let heroData = [
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "indulge", text2: "you passio" },
+    { text1: "dhddjdj", text2: "djdjdj" },
+  ]
+  const [heroCount, setHeroCount] = useState(0);
+  const [playStatus, setPlayStatus] = useState(false);
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount} />
+      <Navbar />
+      <Hero
+        setPlayStatus={setPlayStatus}
+        heroData={heroData}
+        hereCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+      />
+    </div>
+    
+  )
+}
+
+export default App
