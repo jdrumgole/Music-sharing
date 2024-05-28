3c5fdad86e6d08d4f7982c8c735e3d48ca63162a
jdrumgole <85814044+jdrumgole@users.noreply.github.com>
2024-05-28 09:15:11 -0400

    modified:   src/App.jsx 	modified:   src/Components/Background/Background.css 	modified:   src/Components/Background/Background.jsx 	modified:   src/Components/Hero/Hero.css 	modified:   src/Components/Hero/Hero.jsx 	modified:   src/index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Background from "./Components/Background/Background.jsx";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Hero from "./Components/Hero/Hero.jsx";
@@ -12,14 +12,22 @@ const App = () => {
   ]
   const [heroCount, setHeroCount] = useState(0);
   const [playStatus, setPlayStatus] = useState(false);
+
+ //auto
+  useEffect(() => {
+    setInterval(() => {
+      setHeroCount((count) => {return count===2?0:count+1})
+    }, 3000);
+  },[])
+
   return (
     <div>
       <Background playStatus={playStatus} heroCount={heroCount} />
       <Navbar />
       <Hero
         setPlayStatus={setPlayStatus}
-        heroData={heroData}
-        hereCount={heroCount}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
         setHeroCount={setHeroCount}
         playStatus={playStatus}
       />
--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -13,13 +13,13 @@ const Background = ({playStatus,heroCount}) => {
         )
     }
     else if (heroCount === 0) {
-        return <img src={image1} className='background' alt='' />
+        return <img src={image1} className='background fade-in' alt='' />
     }
     else if (heroCount === 1) {
-        return <img src={image2} className='background' alt='' />
+        return <img src={image2} className='background fade-in' alt='' />
     }
     else if (heroCount === 2) {
-        return <img src={image3} className='background' alt='' />
+        return <img src={image3} className='background fade-in' alt='' />
     }
 }
 
